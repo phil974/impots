3807b0f2648e7e3040fb90bc35c4a7b18df8b9b9
Philippe Charton <philippe.charton@univ-reunion.fr>
2016-06-07 16:20:30 +0400

    premier commit pour de vrai

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # test
 only a test repo
+
+Cela devient un essai un peu plus approfondi
---
+++ impots/ui.R
@@ -0,0 +1,20 @@
+shinyUI(fluidPage(
+titlePanel("Quelques éléments sur le système fiscal français"),
+mainPanel(
+    
+    h2("L'impôt sur le revenu (barème sur les revenus de ",di$annee,")"),
+    p("Normalement, l'impôt est progressif.
+Ce qui veut dire, que plus le revenu est important, plus le taux  d'imposition est important."),
+    sliderInput("revenus","revenus par personne (k€)",
+                             min = 10,
+                             max = 200, value = 50),
+        plotOutput("Plot1"),
+    p("revenu d'activité imposable avec frais professionnels de 10 % minimun", 
+      di$plancherFraisPro," maximum ",di$plafondFraisPro),
+    p("Le taux marginal d’impostion (TMI) c’est l’impôt en plus quand on augmente son revenu de 100 €."),
+    
+    p("C’est aussi la pente du graphique ci-dessus."),
+    plotOutput("Plot2")
+    )        
+)
+)
